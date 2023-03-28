429a1f535875e93c2432939561d6855cbc89fc8b
hardikbhanderi <bhanderi8642@gmail.com>
2023-03-28 18:24:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const http = require("http");
+
+// Blocking synchronous way
+// const one = fs.readFileSync('./files/one.txt','utf-8');
+// const newText =  `This is the front text ${one}`;
+// fs.writeFileSync('./files/one.txt',newText);
+// console.log(`File Written! `);
+
+// Non-Blocking Asynchronous way
+// fs.readFile('./files/one.txt','utf-8',(err, data) =>{
+//     console.log(data)
+// })
+// console.log("Will read file!")
+
+const server = http.createServer((req, res) => {
+  const pathname = req.url;
+  if (pathname === "/overview" || pathname === "/") {
+    res.end("Hello This is the OVERVIEW");
+  } else if (pathname === "/product") {
+    res.end("Hello This is the PRODUCT");
+  }else{
+    res.writeHead(404,{
+        "Content-type":'text/html',
+        "my-own-header": "Hello World"
+    })
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Listening to request on port 3000");
+});
