2554367bac7b2a65a7f8883b4d8cd1c21d0149b5
hardikbhanderi <bhanderi8642@gmail.com>
2023-03-28 18:47:43 +0530

    feat json data response

--- index.js
+++ index.js
@@ -19,11 +19,20 @@ const server = http.createServer((req, res) => {
     res.end("Hello This is the OVERVIEW");
   } else if (pathname === "/product") {
     res.end("Hello This is the PRODUCT");
-  }else{
-    res.writeHead(404,{
-        "Content-type":'text/html',
-        "my-own-header": "Hello World"
-    })
+  } else if (pathname === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      console.log(productData);
+      res.writeHead(200, {
+        "Content-type": "application/json"
+      });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "Hello World"
+    });
     res.end("<h1>Page not found!</h1>");
   }
 });
